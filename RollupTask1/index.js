fb580d7462fa001d5c0de013dcc2e565256f0558
29DianaYakubuk <dsjakubuk@gmail.com>
2024-12-28 15:47:24 -0500

    task2

--- RollupTask1/index.js
+++ RollupTask1/index.js
@@ -1,1 +1,9 @@
-console.log('Hello World!')
+import "./index.css";
+import JSImage from "./assets/imageJS.jpg";
+
+const mainTitle = document.createElement("h1");
+mainTitle.textContent = "I love JavaScript";
+const ImageHTML = document.createElement("img");
+ImageHTML.src = JSImage;
+
+document.body.append(mainTitle, ImageHTML);
